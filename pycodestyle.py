9308b57e0b312a42ee1d800c89d6c1b53ab62283
MTH35H4RK <switchayman@gmail.com>
2023-08-12 09:27:09 -0700

    fixed pycode

--- pycodestyle.py
+++ pycodestyle.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 
 """
-This module demonstrates beautifully formatted code that passes pycodestyle checks.
+This is a beautifully formatted code that passes pycodestyle checks.
 """
 
-class prettyMe:
+
+class PrettyMe:
     def __init__(self, value):
         self.value = value
 
     def display(self):
         print(f"Value: {self.value}")
 
+
 def main():
-    example_instance = prettyMe("I am a pretty code!")
+    example_instance = PrettyMe("I am a pretty code!")
     example_instance.display()
+
 
 if __name__ == "__main__":
     main()
