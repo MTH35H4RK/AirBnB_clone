158c89ff7d45f7d62661f83abc0a784cd530edee
MTH35H4RK <switchayman@gmail.com>
2023-08-12 10:00:49 -0700

    console style

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This module demonstrates beautifully formatted code that passes pycodestyle checks.
+"""
+
+
+class PrettyMe:
+    def __init__(self, value):
+        self.value = value
+
+
+    def display(self):
+        print(f"Value: {self.value}")
+
+
+def main():
+    example_instance = PrettyMe("I am a pretty code!")
+    example_instance.display()
+
+
+if __name__ == "__main__":
+    main()
