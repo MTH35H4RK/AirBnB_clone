12f68fb91d2ca28e2534c8a96b6162c348a58dfb
bouarichaKarim <bouaricha.karim@gmail.com>
2023-08-13 22:19:15 +0000

    update console.py

--- console.py
+++ console.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""This is code for the Airbnb console"""
+"""This is code for the AirBnB console"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """THIS is the Console class"""
+    """This is the Console class"""
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """quit is to Exit the program"""
+        """Quit is to Exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """this command is to Exit the program with EOF"""
+        """This command is to Exit the program with EOF"""
         print()
         return True
 
     def dohelp(self, arg):
-        """thi command is to Show help information"""
+        """This command is to Show help information"""
         super().dohelp(arg)
 
     def empty_line(self):
-        """this Do nothing if an empty line was entered"""
+        """This Do nothing if an empty line was entered"""
         pass
 
 
